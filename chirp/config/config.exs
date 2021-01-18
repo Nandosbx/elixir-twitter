4b8e0cecb90fa44a614779f79f1bc1d1d20bdbb5
Fernando <nandosbx@users.noreply.github.com>
2021-01-18 12:28:51 -0300

    First Commit

---
+++ chirp/config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :chirp,
+  ecto_repos: [Chirp.Repo]
+
+# Configures the endpoint
+config :chirp, ChirpWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "kIb9eco4ikkPSrPb8a2/qR2/PKQeAJT7EY9+EeOx0u/vSonMCT0WLaV0rS0PBej5",
+  render_errors: [view: ChirpWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Chirp.PubSub,
+  live_view: [signing_salt: "GCwC1t0U"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
